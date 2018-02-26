615575ce8bcbbdfb1435ae2d56ec1b08ca2f1266
Alberto Güerere <aguerere@users.noreply.github.com>
2018-02-26 16:36:51 -0400

    Feature/cookies addressbook conversations full (#19)
    Add `address_book` report
    Add `conversations_full` report 
    Add `cookies` report
    Add `all` placeholder report

---
+++ tools/reports/address_book.js
@@ -0,0 +1,38 @@
+module.exports.name = 'address_book'
+module.exports.description = 'List all address book records contained in the backup.'
+
+// Specify this reporter requires a backup. 
+// The second parameter to func() is now a backup instead of the path to one.
+module.exports.requiresBackup = true
+
+// Specify this reporter supports the promises API for allowing chaining of reports.
+module.exports.usesPromises = true
+
+module.exports.func = function (program, backup, resolve, reject) {
+  backup.getAddressBook()
+  .then((items) => {
+
+    // Use the configured formatter to print the rows.
+    const result = program.formatter.format(items, {
+      // Color formatting?
+      program: program,
+
+      // Columns to be displayed in human-readable printouts.
+      // Some formatters, like raw or CSV, ignore these.
+      columns: {
+        'ID': el => el.ROWID,
+        'First': el => el.First ? el.First.substring(0,10) + '' : '',
+        'Last': el => el.Last ? el.Last.substring(0,10) + '' : '',
+        'Organization': el => el.organization ? el.organization.substring(0,10) + '' : '',
+        'Phone Work': el => el.phone_work ? el.phone_work.substring(0,14) + '' : '',
+        'Phone Mobile': el => el.phone_mobile ? el.phone_mobile.substring(0,14) + '' : '',
+        'Phone Home': el => el.phone_home ? el.phone_home.substring(0,14) + '' : '',
+        'Email': el => el.email ? el.email.substring(0,28) + '' : ''
+      }
+    })
+
+    // If using promises, we must call resolve()
+    resolve(result)
+  })
+  .catch(reject)
+}
