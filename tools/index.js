8eb1833a3654bc124895b8ba27921ac17536c57d
Alberto Güerere <albertoguerere@gmail.com>
2018-04-10 15:57:24 -0400

    Fix bug, missing comma

--- tools/index.js
+++ tools/index.js
@@ -31,7 +31,7 @@ var reportTypes = {
   'pushstore': require('./reports/pushstore'),
   'calendar': require('./reports/calendar'),
   'facebook_profile': require('./reports/facebook_profile'),
-  'facebook_messenger_friends': require('./reports/facebook_messenger_friends')
+  'facebook_messenger_friends': require('./reports/facebook_messenger_friends'),
   'spotify': require('./reports/spotify')
 }
 
